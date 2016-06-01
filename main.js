e5416294856ee49f6cf63a056ceb6b4a432b80d2
Vit Stanislav <slaweet@seznam.cz>
2016-06-01 21:54:55 +0200

    Many improvements

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 
 
-function main() {
-  d3.csv("data/distractors-2/answers.csv", function(answers) {
-    d3.csv("data/distractors-2/flashcards.csv", function(flashcards) {
+function main(dataDir) {
+  dataDir = '/data/' + dataDir;
+  d3.csv(dataDir + "/answers.csv", function(answers) {
+    d3.csv(dataDir + "/flashcards.csv", function(flashcards) {
       answers = filterData(answers);
       answersByUser = preprocessData(answers, flashcards);
-      renderControlls(answers, answersByUser);
+      renderControlls(answers, answersByUser, renderer.colors);
+      showLegendFunc(renderer.colors);
       render(answersByUser, answers, 'is_correct');
       renderer.render = function(displayKey) {
         render(answersByUser, answers, displayKey);
@@ -13,14 +15,29 @@ function main() {
     });
   });
   var renderer = {
-
+    r : 3,
+    colors : ["#ddd", "red"],
   };
   return renderer;
 }
 
-function renderControlls(answers, answersByUser) {
+function renderControlls(answers, answersByUser, colors) {
   d3.select("#controlls")
-    .append("span").text("Attr to color ");
+    .append("button")
+    .text('Zoom in')
+    .on('click', function(d) {
+      renderer.r++;
+      render(answersByUser, answers);
+    });
+  d3.select("#controlls")
+    .append("button")
+    .text('Zoom out')
+    .on('click', function(d) {
+      renderer.r--;
+      render(answersByUser, answers);
+    });
+  d3.select("#controlls")
+    .append("span").text(" Attr to color ");
   d3.select("#controlls")
     .append("select")
     .on('change', function(d) {
@@ -73,7 +90,7 @@ function preprocessData(data, flashcards) {
   }
   var experimentList = [];
   for (i in byExperiment) {
-    byExperiment[i] = byExperiment[i].sort(sortByLength);
+    //byExperiment[i] = byExperiment[i].sort(sortByLength);
     experimentList.push(byExperiment[i]);
   }
 
@@ -85,10 +102,16 @@ function sortByLength(a, b) {
 }
 
 function render(data, answers, displayKey) {
+  if (displayKey) {
+    renderer.displayKey = displayKey;
+  } else {
+    displayKey = renderer.displayKey;
+  }
   var options = {
     displayKey: displayKey,
     domain : getDomain(answers, displayKey),
-    r : 3,
+    r : renderer.r,
+    colors : renderer.colors,
   };
   d3.select("#data-render").selectAll("span").remove();
   d3.select("#controlls")
@@ -148,6 +171,11 @@ function getTitle(data) {
       27: 'A-4',
       28: 'C-R',
       29: 'C-4',
+      30: 'max_options 4',
+      31: 'max_options 6',
+      32: 'max_options 3',
+      33: 'max_options 2',
+      34: 'max_options 8',
   };
 
   var id = data[0][0].experiment_setup_id;
@@ -157,7 +185,7 @@ function getTitle(data) {
 function drawRow(group, row, y, r, options) {
   var colorScale = d3.scale.linear()
     .domain(options.domain)
-    .range(["red", "green"]);
+    .range(options.colors);
   var circles = group.selectAll("circle")
     .data(row)
     .enter()
@@ -194,11 +222,11 @@ function selectAnswer(d,i) {
 
 function showDetails(d,i) {
   var infoDiv = d3.select("div#info");
-  infoDiv.selectAll('span').remove();
-  infoDiv.selectAll('span')
+  infoDiv.selectAll('div').remove();
+  infoDiv.selectAll('div')
     .data(d3.entries(d))
     .enter()
-    .append('span')
+    .append('div')
     .html(function(dd, i) {
       return dd.key + ': <strong>' + dd.value + '</strong>&nbsp;&nbsp;&nbsp;';
     })
@@ -208,5 +236,46 @@ function showDetails(d,i) {
       renderer.render(dd.key);
     });
 }
+  function showLegendFunc(colors) {
+    var canvas = d3.select("#controlls").append("svg");
+    canvas.attr('class','svg-gradient');
+    var gw = 200;
+    var gh = 20;
+    var offsetY = 10;
+    canvas.attr("width",gw).attr("height",gh + offsetY);
+    var gradient = canvas.append("defs")
+      .append("linearGradient")
+      .attr("id", "gradient")
+      .attr("x1", "0%")
+      .attr("y1", "50%")
+      .attr("x2", "100%")
+      .attr("y2", "50%")
+      .attr("spreadMethod", "pad");
+    gradient.append("stop")
+      .attr("offset", "0%")
+      .attr("stop-color", colors[0])
+      .attr("stop-opacity", 1);
+    gradient.append("stop")
+      .attr("offset", "100%")
+      .attr("stop-color", colors[1])
+      .attr("stop-opacity", 1);
+    canvas.append("rect")
+      .attr("width",gw)
+      .attr("height", gh)
+      .attr("x", 0)
+      .attr("y", offsetY)
+      .attr("stroke", "lightgray")
+      .attr("stroke-width",2)
+      .attr("fill", "url(#gradient)");
+    canvas.append("text")
+      .text("Legenda")
+      .attr("x", 10)
+      .attr("y",450);
+    canvas.append("text")
+      .text("0")
+      .attr("x", 5)
+      .attr("y",500);
+  }
 
-var renderer = main();
+var dataDir = (window.location.hash || 'max-options-count').replace('#', '');
+var renderer = main(dataDir);
